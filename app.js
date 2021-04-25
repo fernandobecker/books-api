d0bd3920b6aaba16dab24dca8e89c814a501195c
Fernando <fernando.silva@agriness.com>
2021-04-25 18:42:22 -0300

    Add endpoint to find book

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ app.get("/api/books", (req, resp) => {
     resp.send(books);
 });
 
+app.get("/api/books/:id", (req, resp) => {
+    const book = books.find(b => { b.id === parseInt(req.params.id) })
+    if(!book){ resp.sendStatus(404).send("Book not found"); }
+    resp.send(book);
+});
+
 app.listen(port, function(){
     console.log("info",'Server is running at port : ' + port);
 });
